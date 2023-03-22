0ef85706d414bee0b61fdeb9075a7cb83f496302
TheDeafOne <woodburnkb20@gcc.edu>
2023-03-22 18:04:53 -0400

    app setup for sigin, login, and profile view

--- schedulite.clientapp/src/App.tsx
+++ schedulite.clientapp/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './sceens/HomeScreen/HomeScreen';
-// import Profile from './sceens/ProfileScreen/profile.screen';
-// import Login from './sceens/LoginScreen/login.screen';
+import Profile from './sceens/ProfileScreen/profile.screen';
+import Login from './sceens/LoginScreen/login.screen';
 import Signup from './sceens/SignupScreen/signup.screen';
 
 
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
