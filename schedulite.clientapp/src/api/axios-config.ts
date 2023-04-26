ed354b02948cf4cddd699a279051e30ed2439dd2
TheDeafOne <woodburnkb20@gcc.edu>
2023-04-26 18:11:52 -0400

    axios configuration

--- schedulite.clientapp/src/api/axios-config.ts
+++ schedulite.clientapp/src/api/axios-config.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'http://localhost:8080/api/v1',
     headers: { "ngrok-skip-browser-warning": "false" }
 })
+instance.interceptors.request.use(request => {
+    return request;
+}, error => {
+    return Promise.reject(error);
+});
+export default instance;
