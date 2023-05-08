1b17c409ee384df4bdd1f499b42aede2fd8deede
TheDeafOne <woodburnkb20@gcc.edu>
2023-05-07 23:00:57 -0400

    no years past 2020

--- schedulite.clientapp/src/components/Modals/SetScheduleModal.tsx
+++ schedulite.clientapp/src/components/Modals/SetScheduleModal.tsx
@@ -73,8 +73,6 @@ const SetScheduleModal = ({ setIsOpen }: any) => {
                     <option value="2018">2018</option>
                     <option value="2019">2019</option>
                     <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
                 </select>
             </div>
 
